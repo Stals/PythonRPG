8eecc6ecca039ef8a0c36e6ef02e41e12fb5ce4b
Stals <superstals@gmail.com>
2011-06-28 22:43:49 +0400

    refactored entraceLocation

--- generateDungeon.py
+++ generateDungeon.py
@@ -1,7 +1,12 @@
 import sys
 import random
+#размер Подземелья
 x=100;
 y=10;
+#Координаты входа в подземелье
+entranceLocationX=0;
+entranceLocationY=0;
+
 #создаём подземелье 100 на 10 (x=100,y=10)
 dungeon=[["*"]*x for i in range(y)]#сделано так а не [["*"]*100]*10 чтобы они не ссылись на одни и теже элементы.
 # потому что без этого если бы я сделал dungeon[0][0]=" " , то во всех строках первый элемент стал бы пробелос.
@@ -18,13 +23,21 @@ def printDungeon():
 #определим на какой из сторон будет вход
 entranceWall=random.randint(0,3)
 #Где 0 - Север 1 - Восток 2 - Юг 3 - Запад
-
+#Нарисуем E - Entrance на случайном месте той стророны что выбрали
 if entranceWall==0 :
-    dungeon[0][random.randint(0,x-1)]="E"
+    entranceLocationX=0
+    entranceLocationY=random.randint(0,x-1)
 elif entranceWall==1 :
-    dungeon[random.randint(0,y-1)][x-1]="E"
+    entranceLocationX=random.randint(0,y-1)
+    entranceLocationY=x-1
 elif entranceWall==2:
-    dungeon[y-1][random.randint(0,x-1)]="E"
+    entranceLocationX=y-1
+    entranceLocationY=random.randint(0,x-1)
 elif entranceWall==3:
-    dungeon[random.randint(0,y-1)][0]="E"
+    entranceLocationX=random.randint(0,y-1)
+    entranceLocationY=0
+dungeon[entranceLocationX][entranceLocationY]="E"
+
+
+
 printDungeon()
