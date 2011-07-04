979681c8734e06cf6d7a520f679ab43d54ddddfd
Stals <superstals@gmail.com>
2011-07-04 21:44:38 +0400

    comments updated

--- usefullFunctions.py
+++ usefullFunctions.py
@@ -1,4 +1,5 @@
 #Задаёт задаёт question и возвращает выбраннй вариант ответа из данного списка вариантов (choices)
+#Тоесть человек выбирает вариант цифрами но возвращается один из указанных вариантов
 ## Использование : result=getChoice("Are You Sure?",["Yes","No","Not Sure"])
 ## if result=="Yes":
 ## #doSomthing
@@ -9,7 +10,6 @@ def getChoice(question , choices):
         print(question)
         for i in range(1,len(choices)+1):
             print("{0}. {1}".format(i,choices[i-1]))
-        #TODO DO WHILE в PYHTHON
         while True: #Бесконечный цикл до тех пор пока не будет правильный ввод
             result=input("Your choice is: ")
             if result.isdigit():
@@ -21,10 +21,11 @@ def getChoice(question , choices):
 
 
 
-#result = getChoice("are you ok?",["yes","no"])
-#сразу запихнуть результат в race внутри класса hero
+
+##сразу запихнуть результат в race внутри класса hero
+#def getRace():
 #self.Race=getChoice("Choose your Race:",["Human","Were-Wolf","Demon","Elf"])
-#после чего уже в зависимости от варианта  сделать статы
+##после чего уже в зависимости от варианта  сделать статы
 #if self.Race=="Human"
 #    pass
 #elif self.Race=="Were-Worf"
