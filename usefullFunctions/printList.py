dfdf6b1de807ca7c592090fcf4d845c3b5e947f5
Stals <superstals@gmail.com>
2011-07-06 15:42:41 +0400

    one more usefull function

---
+++ usefullFunctions/printList.py
@@ -0,0 +1,9 @@
+#Допустим есть список Монстров, вы кидаете его в это функцию и получаете список всех монстров
+def printList(list):
+    if len(list)>=1:
+        for i in range(1,len(list)+1):
+            print("{0}. {1}".format(i,list[i-1].__str__()))
+
+
+monsters=["Dragon","Mighty Wizard","Black Knight"]
+printList(monsters)
