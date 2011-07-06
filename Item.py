52139c52e4bc2d601bb70d3f0a3b69d5ca0b06ad
Stals <superstals@gmail.com>
2011-07-06 18:44:47 +0400

    Item.py added comments

--- Item.py
+++ Item.py
@@ -1,3 +1,4 @@
+#функция __str__() в класса позволяет очень просто выводит их содержимое через print(MagicBow) например
 import random
 
 #Позволяет не думать о том какие статы сейчас есть, за счёт того что они пределены только в одном месте
@@ -20,6 +21,7 @@ class damage:
         return random.randint(self.min,self.max)
 
 #Необходим только для вещей (Брони/Оружия) так что он останется тут
+#bonusStats является оберткой вокруг stats чтобы
 class bonusStats:
     def __init__(self,stats):
         self.stats=stats
@@ -36,11 +38,20 @@ class bonusStats:
 
 
 class Item:
+    #TODO сделать getStr() и другие методы если будет необходимо
     def __init__(self,name,stats):
         self.name=name
-        #TODO перенести отдельно в Armour и Weapon
+        #TODO перенести отдельно в Armour и Weapon , если potion будет наследоваться от item
         self.bonusStats=bonusStats(stats)#Вещь может иметь статы
     def __str__(self): #переопределен в наследниках
+        pass
+    #TODO Так как equip для каждого куска брони будет уникален, в него можно заложить каую именно часть он будет снимать и куда надеваться
+    #TODO Написать в Human функции addBonusStats и deleteBonusStats
+    def equip(self,hero):
+        #Снимает вещь в томже слоте делая takeOff после чего одевает Item и добавляет bonusStats к статистикам игрока
+        pass
+    def takeOff(self,hero):
+        #Снимает вещь, убирая те эффекты которые она давала и кладёт её в инвентарь
         pass
 
 
@@ -55,5 +66,5 @@ class Weapon(Item):
 
 magicBow=Weapon("Magic Bow",stats(1,3,3,4),damage(5,10))
 fireSword=Weapon("Fire Sword",stats(1,2,3,4),damage(1,10))
-
+print(fireSword.damage.getDamage())
 
