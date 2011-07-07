286bb6a43ae79ece39175e375b2db48201b06ec4
Stals <superstals@gmail.com>
2011-07-07 23:17:32 +0400

    do not need __str__() either

--- usefullFunctions/printList.py
+++ usefullFunctions/printList.py
@@ -2,7 +2,7 @@
 def printList(list):
     if len(list)>=1:
         for i in range(1,len(list)+1):
-            print("{0}. {1}".format(i,list[i-1].__str__()))
+            print("{0}. {1}".format(i,list[i-1]))
 
 
 monsters=["Dragon","Mighty Wizard","Black Knight"]
