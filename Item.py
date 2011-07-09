567c09bac6860ce7577aa5e5861265b908ef1397
Stals <superstals@gmail.com>
2011-07-09 18:43:07 +0400

    added armour type to a Armour class to understand were should it be equiped

--- Item.py
+++ Item.py
@@ -50,17 +50,21 @@ class Weapon(Item):
         return '"{0}" {1} {2}'.format(self.name,self.damage,self.bonusStats)
 
 class Armour(Item):
-    def __init__(self,name,stats,defence):
+
+    def __init__(self,name,stats,defence,armourType):
         super().__init__(name,stats)
         self.defence=defence
+        self.armourType=armourType
     def __str__(self):
         return '"{0}"  Defence:{1} {2}'.format(self.name,self.defence,self.bonusStats)
 
 
+
+#TODO убрать отсюда
 magicBow=Weapon("Magic Bow",stats(1,3,3,4),damage(5,10))
 fireSword=Weapon("Fire Sword",stats(1,2,3,4),damage(1,10))
 
-ironBoots=Armour("Iron Boots",stats(3,2,0,0),10)
+ironBoots=Armour("Iron Boots",stats(3,2,0,0),10,"Boots")
 
-print(fireSword)
-print(ironBoots)
+#print(fireSword)
+#print(ironBoots)
