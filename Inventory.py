410de44cfd7538da9e664b2f1c1eb7d6fb0c7ccb
Stals <superstals@gmail.com>
2011-07-16 13:46:19 +0400

    changed removeItem method . Now it removes item from a list if it is in it.

--- Inventory.py
+++ Inventory.py
@@ -1,18 +1,20 @@
 from Item import Item
 #Хранит вещи игрока
+
+#TODO сделать максимально переносимое число вещей ( зависит от Stamina )
 class Inventory:
     inventory = []
     def __int__(self):
         pass
     def addItem(self,newItem):
         self.inventory.append(newItem)
-    def removeItem(self,index):# Индекс начинается с 0
-        self.inventory.pop(index)
-    def sort(self):
+        self.__sort()
+    def removeItem(self,item):# if item is in invenrory it will be removed
+        if item in self.inventory:
+            self.inventory.remove(item)
+    def __sort(self):
         #TODO сделать сортировку по типу Ботинки вместе, оружие вместе и тд
         pass
-
-
     def __str__(self):
         result = ""
         if len(self.inventory)!=0:
