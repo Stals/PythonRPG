3c3594adb692248188c8d2d9417807467f95dcd1
Stals <superstals@gmail.com>
2011-07-16 12:37:17 +0400

    added getChoiceID function

--- usefullFunctions/getChoice.py
+++ usefullFunctions/getChoice.py
@@ -46,3 +46,18 @@ def getChoice(question , choices):
 ## print("You choosed a {0}".format(choosedItem.name))
 ###После чего можно просто сделать
 ## choosedItem.equip() # если все эти вещи определены в классе
+
+
+# returns 0 for the first element and so on
+def getChoiceId(question , choices):
+    if len(choices)>=1:
+        print(question)
+        for i in range(1,len(choices)+1):
+            print("{0}. {1}".format(i,choices[i-1]))# Для вывода элемента из choices вызывается __str__()
+        while True: #Бесконечный цикл до тех пор пока не будет правильный ввод
+            result=input("Your choice is: ")
+            if result.isdigit():
+                if 1 <= int(result) <= len(choices):
+                    return int(result)-1
+    else:
+        return -1
