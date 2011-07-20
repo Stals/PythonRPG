62d175379ec44124ec81a8a9e95915dddf20810f
Stals <superstals@gmail.com>
2011-07-20 14:36:07 +0400

    added bonusDefence for monster

--- Monster.py
+++ Monster.py
@@ -1,14 +1,14 @@
 from Entity import *
 class Monster(Entity):
-    def __init__(self,name,stats,bonusDamage,resists,gold=0):
+    def __init__(self,name,stats,bonusDamage,bonusDefence,resists,gold=0):
         super().__init__(name,stats,resists,gold)
         self.damage.addDamage(bonusDamage) #TODO Убрать если у монстров будет оружие
-
+        self.defence=bonusDefence#TODO убрать если у монстров будет броня
     def doTurn(self):#used by Battle to make one turn
         pass
 
 
 #initialization looks like that
-#monster = Monster("Dragon",Stats(5,5,5,5),Damage(1,10),Elements(0,0,0,0),10)
-#                    |              |              |               |       |
-#                  name           Stats        bonusDamage      resists  gold
+#monster = Monster("Dragon",Stats(5,5,5,5),Damage(1,10),5,Elements(0,0,0,0),10)
+#                    |              |        |          |            |       |
+#                  name           Stats  bonusDamage  bonusDefence resists  gold
