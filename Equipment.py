00ad1c0236504534a138eb56fea5f6788aaa3096
Stals <superstals@gmail.com>
2011-07-20 13:55:41 +0400

    eqipment now returns a list of all his elements

--- Equipment.py
+++ Equipment.py
@@ -1,3 +1,4 @@
+#чтобы предложить снять вещь из equipment'а нужео передать items() в getChoice()
 class Equipment:
     def __init__(self):
          self.equipment={
@@ -8,6 +9,14 @@ class Equipment:
             "Leggings" : "empty",
             "Boots"    : "empty"
         }
+    def items(self):#returns list of all items that are equiped
+        list = []
+
+        for piece,item in self.equipment.items():
+            if item!="empty":
+                list.append(item)
+        return list
+
     def __str__(self):
         result = ""
         for piece,item in self.equipment.items():
