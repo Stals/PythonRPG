76aee9e01d039c04405f0885169a85a5cdff6ed8
Stals <superstals@gmail.com>
2011-11-10 14:51:40 +0400

    rewrote some code in getChoices

--- usefullFunctions/getChoice.py
+++ usefullFunctions/getChoice.py
@@ -9,31 +9,33 @@
 # Если была выбрана Отмена (Cancel) будет возвращен 0
 # В случае ошибки будет возвращен -1
 
-
-
 #TODO защиту от русских букв
 def getChoice(question , choices , cancel = False):
-    if len(choices)>=1:
-        print(question)
-        if cancel == True:
-                print ("0. Cancel")
-        for i in range(1,len(choices)+1):
-            print("{0}. {1}".format(i,choices[i-1]))# Для вывода элемента из choices вызывается __str__()
+	if len(choices)>=1:
+		print(question)
+		if cancel == True:
+				print ("0. Cancel")
+		for i in range(1, len(choices) + 1):
+			print("{0}. {1}".format(i, choices[i - 1]))# Для вывода элемента из choices вызывается __str__()
 
-        while True: #Бесконечный цикл до тех пор пока не будет правильный ввод
-            result=input("Your choice is: ")
-            if result.isdigit():
-                if cancel == True: # Если также возможен вариант отмены
-                    if 0 <= int(result) <= len(choices):
-                        if int(result) == 0 :
-                            return 0
-                        else:
-                            return choices[int(result)-1]
-                else:
-                    if 1 <= int(result) <= len(choices):
-                        return choices[int(result)-1]
-    else:
-        return -1
+		# Бесконечный цикл до тех пор пока не будет правильный ввод
+		while True:
+			result = input("Your choice is: ")
+			if result.isdigit():
+				if not cancel:
+					# Если вариант отмены не предусмотрен
+					if 1 <= int(result) <= len(choices):
+						return choices[ int(result) - 1 ]
+
+				elif cancel:
+					# Если возможен вариант отмены разрешаем вариант ввода нуля
+					if 0 <= int(result) <= len(choices):
+						if int(result) == 0 :
+							return 0
+						else:
+							return choices[ int(result) - 1 ]
+	else:
+		return -1
 
 
 
@@ -73,14 +75,14 @@ def getChoice(question , choices , cancel = False):
 #TODO перемсотреть необходимость
 # returns 0 for the first element and so on
 def getChoiceId(question , choices):
-    if len(choices)>=1:
-        print(question)
-        for i in range(1,len(choices)+1):
-            print("{0}. {1}".format(i,choices[i-1]))# Для вывода элемента из choices вызывается __str__()
-        while True: #Бесконечный цикл до тех пор пока не будет правильный ввод
-            result=input("Your choice is: ")
-            if result.isdigit():
-                if 1 <= int(result) <= len(choices):
-                    return int(result)-1
-    else:
-        return -1
+	if len(choices)>=1:
+		print(question)
+		for i in range(1,len(choices)+1):
+			print("{0}. {1}".format(i,choices[i-1]))# Для вывода элемента из choices вызывается __str__()
+		while True: #Бесконечный цикл до тех пор пока не будет правильный ввод
+			result=input("Your choice is: ")
+			if result.isdigit():
+				if 1 <= int(result) <= len(choices):
+					return int(result)-1
+	else:
+		return -1
