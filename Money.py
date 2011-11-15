d789af9d487315fee3b6d536779b48908b82ca90
Stals <superstals@gmail.com>
2011-11-15 11:11:02 +0400

    wrote a class to store and manage money

---
+++ Money.py
@@ -0,0 +1,107 @@
+#NOTE перед операциями - + , нужно проверять хватает ли денег с помощью > < >= <=
+
+# 100 бронзы = 1 серебро
+# 100 серебра = 1 золото
+class Money: #TODO сделать Exceptions Если при вычетании и сложении
+	"""
+	Класс Money позволяет хранить и проихводить операции над деньгами
+	Серебро и бронза должно быть инициализировано числом от 0 до 99 ,
+	а золото любым числом от 0.
+	Сложение и вычитание бронзы может менять серебро, а серебра - золото
+	"""
+	def __init__(self, gold=0, silver=0, bronze=0):
+		if gold>=0:
+			self.gold = gold
+		else:
+			raise Exception("Wrong gold value!")
+
+		if 100 > silver >= 0:
+			self.silver = silver
+		else:
+			raise Exception("Wrong silver value!\nShould be between 0 and 99")
+
+		if 100 > silver >=0:
+			self.bronze = bronze
+		else:
+			raise Exception("Wrong bronze value!\nShould be between 0 and 99")
+
+	def __str__(self):
+		return "{0} gold {1} silver {2} bronze".format(self.gold, self.silver, self.bronze)
+
+	def toBronze(self):
+		"""
+		Возвращает кол-во денег в виде бронзы
+		"""
+		return self.gold*100*100 + self.silver*100 + self.bronze
+
+	# >=
+	def __ge__(self, other): #TODO mb проверять хватает ли золота перед конвертацией
+		# Сравним Кол-во денег в Бронзовом эквиваленте
+		if self.toBronze() >= other.toBronze():
+			return True
+		else:
+			return False
+
+	# >
+	def __gt__(self, other):
+		return not self.__le__(other)
+
+	# <
+	def __lt__(self, other):
+		return not self.__ge__(other)
+
+	# <=
+	def __le__(self, other):
+		# Сравним Кол-во денег в Бронзовом эквиваленте
+		if self.toBronze() <= other.toBronze():
+			return True
+		else:
+			return False
+
+	# +
+	def __add__(self, other):
+		self.bronze += other.bronze
+		if self.bronze > 100:
+			self.bronze -= 100
+			self.silver += 1
+		self.silver += other.silver
+		if self.silver > 100:
+			self.silver -= 100
+			self.gold += 1
+		self.gold += other.gold
+		return self
+
+	# +=
+	def __iadd__(self, other):
+		self.__add__(other)
+		return self
+
+	# -
+	def __sub__(self, other):
+		self.bronze -= other.bronze
+		if self.bronze < 0:
+			self.bronze += 100
+			self.silver -= 1
+		self.silver -= other.silver
+		if self.silver < 0:
+			self.silver += 100
+			self.gold -= 1
+		self.gold -= other.gold
+		return self
+
+	# -=
+	def __isub__(self, other):
+		self.__sub__(other)
+		return self
+
+	# ==
+	def __eq__(self, other):
+		if self.gold == other.gold and\
+			self.silver == other.silver and\
+			self.bronze == other.bronze:
+			return True
+		else:
+			return False
+	# !=
+	def __ne__(self, other):
+		return not self.__eq__(other)
