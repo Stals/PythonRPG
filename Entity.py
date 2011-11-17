c4a69a3368c36d89c7b1c267b102b0faf6f944b9
Stals <superstals@gmail.com>
2011-11-17 15:20:38 +0400

    spaces and comments

--- Entity.py
+++ Entity.py
@@ -4,15 +4,13 @@ import random
 class Entity:
 
     def __init__(self,name,stats,resists,money):
-
         self.name = name
         self.damage = Damage()
         self.hp = 0
         self.maxHp = 0
         self.mp = 0
         self.maxMp = 0
-        ## Показатель защиты
-        self.defence = 0
+        self.defence = 0 # Показатель защиты
         self.stats = Stats()
         self.stats.addStats(stats, self)
 
@@ -28,12 +26,11 @@ class Entity:
     def simpleAttack(self,target):#simple attack with a weapon // returns true if this killed an enemy  #TODO писать каким оружием бъеш - только имя (а если оружие не надаето ==empty писать что бъеш руками)
         #TODO тут действуют только defence и damage(weapon+str) , а dex будет влиять в battle на то как часто ты бъеш...
         #Разность в Dex влияет на попадание
-        hitChance=self.stats.dex()/target.stats.dex()
+        hitChance = self.stats.dex() / target.stats.dex()
         if hitChance > 1 or random.random() < hitChance: #Если у вас Dex больше или random попал в шанс попадания
             dmg=self.damage.getDamage()-round(target.defence/2)
             if dmg < 0:
                 dmg = 0
-
             target.hp-=dmg
             if target.isDead():
                 print('"{0}" kills "{1}" with {2} damage.'.format(self.name,target.name,dmg))
