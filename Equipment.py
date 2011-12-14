63204ee165400ed013ad0079196d5713a1605293
Stals <superstals@gmail.com>
2011-12-14 09:17:17 +0400

    changed spaces to tabs

--- Equipment.py
+++ Equipment.py
@@ -2,35 +2,36 @@
 
 ## Обмундирование 
 class Equipment:
-    def __init__(self):
-        ## Хранит обмундирование в виде Словаря
-         self.equipment={
-            "Weapon"   : "empty",
-            "Head"     : "empty",
-            "Gloves"   : "empty",
-            "Chest"    : "empty",
-            "Leggings" : "empty",
-            "Boots"    : "empty"
-        }
+	def __init__(self):
+	## Хранит обмундирование в виде Словаря
+		self.equipment={
+			"Weapon"   : "empty",
+			"Head"     : "empty",
+			"Gloves"   : "empty",
+			"Chest"    : "empty",
+			"Leggings" : "empty",
+			"Boots"    : "empty"
+		}
 
-    ## Возвращает список всех вещей которые одеты
-    def items(self):
-        list = []
-        for piece,item in self.equipment.items():
-            if item != "empty":
-                list.append(item)
-        return list
+	## Возвращает список всех вещей которые одеты
+	def items(self):
+		list = []
+		for piece,item in self.equipment.items():
+			if item != "empty":
+				list.append(item)
+		return list
 
-    ## Возвращает имя оружия если оно есть, иначе возвращает Fists (кулаки)
-    def weapon(self):
-        if self.equipment["Weapon"] == "empty":
-            return "Fists"
-        else:
-            return self.equipment["Weapon"].name
+	## Возвращает имя оружия если оно есть, иначе возвращает Fists (кулаки)
+	def weapon(self):
+		if self.equipment["Weapon"] == "empty":
+			return "Fists"
+		else:
+			return self.equipment["Weapon"].name
 
-    ## Возвращает все одетые вещи в виде строки
-    def __str__(self): #TODO! красивый вывод
-        result = ""
-        for piece,item in self.equipment.items():
-            result+="{0} : {1}\n".format(piece,item)
-        return result
+	## Возвращает все одетые вещи в виде строки
+	def __str__(self): #TODO! красивый вывод
+		result = ""
+		for piece,item in self.equipment.items():
+			result+="{0} : {1}\n".format(piece,item)
+		return result
+
