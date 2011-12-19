cacd57f1a041389e0a18708c33c4a026dec9a1cd
Stals <superstals@gmail.com>
2011-12-19 12:42:34 +0400

    todo added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from Item import *
 from Battle import *
 from Stats import *
 
-
+#TODO! Добавить canUse методы в другие классы (weapon,armor - может можно просто в ITEM)
 #TODO! внутри spell.__str__() выводится имя спелла и Описани того что он делает( зависит от спелла )
 
 player = Hero()
