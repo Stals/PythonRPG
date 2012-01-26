288361fc5aaec728d0a85fa2e4cd9cd08ccf1867
Stals <superstals@gmail.com>
2012-01-26 21:11:00 +0400

    if entity missed it will still show enemies Hp left

--- Entity.py
+++ Entity.py
@@ -32,7 +32,7 @@ class Entity:
 				print('"{0}" hits "{1}" for {2} damage. [{3} hp left]'.format(self.name,target.name,dmg,target.stats.hp))
 				return False
 		else: #you missed
-			print('"{0}" missed "{1}" with {2}% hit chance.'.format(self.name, target.name, round(hitChance*100)))
+			print('"{0}" missed "{1}" with {2}% hit chance. [{3} hp left]'.format(self.name, target.name, round(hitChance*100),target.stats.hp))
 
 	## Возвращает true если Entity мертв
 	def isDead(self):
